1e5b062c9d9d7455808f536456cf15ca26e49501
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2023-07-22 16:38:48 +0400

    VAE implementation start: rule based agent added

--- agents.py
+++ agents.py
@@ -63,3 +63,86 @@ class Agent(object):
 		with torch.no_grad():
 			action_space = self.body.forward(x)
 			return torch.argmax(action_space)-1
+
+
+class Rule_based_agent(object):
+	"""Rule_based_agent could be used for 9x9 field only"""
+	def __init__(self, mask_id):
+		from collections import deque
+		from random import randint
+
+		if mask_id=='1':
+			self.mask = deque([
+				((1,1), -1),
+				((1,7), -1),
+				((2,7), -1),
+				((2,2), 1),
+				((3,2), 1),
+				((3,7), -1),
+				((4,7), -1),
+				((4,2), 1),
+				((5,2), 1),
+				((5,7), -1),
+				((7,7), -1),
+				((7,6), -1),
+				((6,6), 1),
+				((6,5), 1),
+				((7,5), -1),
+				((7,4), -1),
+				((6,4), 1),
+				((6,3), 1),
+				((7,3), -1),
+				((7,2), 'choice')
+			])
+		elif mask_id=='2':
+			pass
+			# self.mask = 
+		elif mask_id=='3':
+			pass
+			# self.mask = 
+		else:
+			raise ValueError
+
+		self.mask_id = mask_id
+		self.position, self.action = self.get_condition((-1,-1))
+
+	def choice(self, food_position):
+		if self.mask_id=='1':
+			ROUTES = {-1: deque([((6,2), 1), 
+								 ((6,1), -1)]), 
+					  0 : deque([((7, 1), -1)])}
+			if food_position in [(6,2), (7,1)]:
+				if food_position in [(6,2)]:
+					action = -1
+				else:
+					action = 0
+			else:
+				action = randint(-1,0)
+			self.chosen_route = ROUTES[action]
+			return action
+
+		elif self.mask_id=='2':
+			pass
+		elif self.mask_id=='3':
+			pass
+		else:
+			raise ValueError
+	
+	def get_condition(self, food_position):
+		if not self.chosen_route:
+			condition = self.mask.popleft()
+			self.mask.append(condition)
+			pos, act = condition
+			if act=='choice':
+				act = self.choice(food_position)
+		else:
+			pos, act = self.chosen_route.popleft()
+		return pos, act
+
+	def get_action(self, head_position, food_position):
+		if head_position not in [self.position]:
+			return 0
+		else:
+			output = self.action
+			self.position, self.action = self.get_condition(food_position)
+			return output
